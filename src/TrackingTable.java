b63707da2e04fcb352ae2621e4829633ff530b76
ekanshmittal <ontoekansh@yahoo.com>
2015-07-27 18:32:36 +0530

    added display function to tracking table

--- src/TrackingTable.java
+++ src/TrackingTable.java
@@ -1,9 +1,21 @@
-
 import java.util.*;
 
+public class TrackingTable {
+	private List<TrackingRow> table;
 
-public class TrackingTable{
-	List<TrackingRow> row;
-	
+	public TrackingTable() {
+		table = new ArrayList<TrackingRow>();
+	}
+
+	public void addRow(TrackingRow row) {
+		table.add(row);
+	}
+	public void displayTable(){
+		Iterator<TrackingRow> it = table.iterator();
+		while(it.hasNext()){
+			TrackingRow row = it.next();
+			System.out.println(row.getRange().toString()+" "+row.getStatusCode()+" "+row.getTransferCode());
+		}
+	}
 	
 }
