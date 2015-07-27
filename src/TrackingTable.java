0083941644d1e078037fc37f12fa6feaa6c231da
ekanshmittal <ontoekansh@yahoo.com>
2015-07-27 18:36:59 +0530

    added add row

--- src/TrackingTable.java
+++ src/TrackingTable.java
@@ -5,17 +5,23 @@ public class TrackingTable {
 
 	public TrackingTable() {
 		table = new ArrayList<TrackingRow>();
+
+		TrackingRow row = new TrackingRow(new Range(1, Integer.MAX_VALUE),
+				null, null);
+		table.add(row);
 	}
 
 	public void addRow(TrackingRow row) {
 		table.add(row);
 	}
-	public void displayTable(){
+
+	public void displayTable() {
 		Iterator<TrackingRow> it = table.iterator();
-		while(it.hasNext()){
+		while (it.hasNext()) {
 			TrackingRow row = it.next();
-			System.out.println(row.getRange().toString()+" "+row.getStatusCode()+" "+row.getTransferCode());
+			System.out.println(row.getRange().toString() + " "
+					+ row.getStatusCode() + " " + row.getTransferCode());
 		}
 	}
-	
+
 }
