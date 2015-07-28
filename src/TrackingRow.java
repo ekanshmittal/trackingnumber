16a8972749225c5d34c10e07b9347083a72d4669
Bhavya V. Karki <bhavyakarki@gmail.com>
2015-07-28 09:19:44 +0530

    modified trackingrow.java

--- src/TrackingRow.java
+++ src/TrackingRow.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class TrackingRow {
 	private Range range;
-	private Character statusCode;
-	private Integer transferCode;
+	private char statusCode;
+	private int transferCode;
 	private boolean deleted;
 
 	public TrackingRow(Range range, Character statusCode, Integer transferCode) {
@@ -43,12 +43,27 @@ public class TrackingRow {
 	
 	public void addRow(ArrayList<TrackingRow> table,TrackingRow newEntry) {
 		for( TrackingRow tableEntry : table) {
-			 Range.Relation relation = tableEntry.classify(newEntry);
+			 Range.Relation relation = newEntry.classify(tableEntry);
 			 
 			 if( relation == Range.Relation.SUBSET ) {
+				 if(newEntry.statusCode == tableEntry.statusCode  &&  newEntry.transferCode == tableEntry.transferCode)
+					 newEntry.deleted = true;
+					 
+					 else{
+						 tableEntry.range.hi = newEntry.range.lo - 1;
+						 Range newRange = new Range(newEntry.range.hi+1,tableEntry.hi)
+						 TrackingRow splitEntry = new TrackingRow(newRange,tableEntry.statusCode,tableEntry.transferCode);
+						 ArrayList<TrackingRow> copyList = new 
+					 }		
 				 
 			 }
-			 
+			 if( relation == Range.Relation.SUPERSET ) {
+				  
+				 
+			 }
+			 if( relation == Range.Relation.LESSOVERLAP ){
+				 add
+			 }
 			 
 		}
 	}
