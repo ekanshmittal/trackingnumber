91d81de779bcf92ca2bd0ee8912f69e0da319b05
Ekansh Mittal <ontoekansh@yahoo.com>
2015-07-28 10:06:39 +0530

    changes to iterator

--- src/TrackingRow.java
+++ src/TrackingRow.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class TrackingRow {
 	private Range range;
@@ -10,7 +11,7 @@ public class TrackingRow {
 		this.range = range;
 		this.statusCode = statusCode;
 		this.transferCode = transferCode;
-		deleted =  false ; 
+		deleted = false;
 	}
 
 	public Range getRange() {
@@ -40,45 +41,46 @@ public class TrackingRow {
 	public Range.Relation classify(TrackingRow tr) {
 		return getRange().classify(tr.getRange());
 	}
-	
-	public void addRow(ArrayList<TrackingRow> table,TrackingRow newEntry) {
-		
-		 ArrayList<TrackingRow> copyList = new ArrayList<TrackingRow>();
-		 copyList = table;
-		for( TrackingRow tableEntry : table) {
-			 Range.Relation relation = tableEntry.classify(newEntry);
-			 
-			 if( relation == Range.Relation.SUBSET ) {
-				 if(isEqual(newEntry))
-					 newEntry.deleted = true;
-					 
-					 else{
-						 tableEntry.getRange().hi = newEntry.getRange().lo - 1;
-						 Range newRange = new Range(newEntry.getRange().hi+1,tableEntry.getRange().hi)
-						 TrackingRow splitEntry = new TrackingRow(newRange,tableEntry.statusCode,tableEntry.transferCode);
-						
-						 copyList.add(splitEntry);
-						 copyList.add(newEntry);
-						 
-					 }		
-				 
-			 }
-			 if( relation == Range.Relation.SUPERSET ) {
-				  
-				 
-			 }
-			 if( relation == Range.Relation.LESSOVERLAP ){
-				 
-			 }	
-			 if(relation == Range.Relation.SAME){
-				 if(isEqual(newEntry)){
-					 statusCode=newEntry.statusCode;
-					 transferCode=newEntry.transferCode;
-				 }
-			 }
-		}	
+
+	public void addRow(ArrayList<TrackingRow> table, TrackingRow newEntry) {
+
+		ListIterator<TrackingRow> listIterator = table.listIterator();
+		while (listIterator.hasNext()) {
+			TrackingRow tableEntry = listIterator.next();
+			Range.Relation relation = tableEntry.classify(newEntry);
+
+			if (relation == Range.Relation.SUBSET) {
+				if (isEqual(newEntry))
+					newEntry.deleted = true;
+
+				else {
+					tableEntry.getRange().hi = newEntry.getRange().lo - 1;
+					Range newRange = new Range(newEntry.getRange().hi + 1,
+							tableEntry.getRange().hi);
+					TrackingRow splitEntry = new TrackingRow(newRange,
+							tableEntry.statusCode, tableEntry.transferCode);
+
+					copyList.add(splitEntry);
+					copyList.add(newEntry);
+
+				}
+
+			}
+			if (relation == Range.Relation.SUPERSET) {
+			}
+			if (relation == Range.Relation.LESSOVERLAP) {
+
+			}
+			if (relation == Range.Relation.SAME) {
+				if (isEqual(newEntry)) {
+					statusCode = newEntry.statusCode;
+					transferCode = newEntry.transferCode;
+				}
+			}
+		}
 	}
-	public boolean isEqual(TrackingRow tr){
-		return statusCode == tr.statusCode  &&  transferCode == tr.transferCode;
+
+	public boolean isEqual(TrackingRow tr) {
+		return statusCode == tr.statusCode && transferCode == tr.transferCode;
 	}
 }
